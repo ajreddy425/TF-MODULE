0dd00fbd0095b5a1ba2e9e6811d596e12977808e
ajreddy425 <82323282+ajreddy425@users.noreply.github.com>
2021-06-19 17:04:53 +0530

    Update ec2.tf

--- module/instances/ec2.tf
+++ module/instances/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "my_ec2" {
 count           =var.ec2_count
   ami           = var.my_ami
   instance_type = var.ins_type
-  subnet_id     = var.subnet_id
+#   subnet_id     = var.subnet_id
 
   tags = {
     Name = var.ec2_tags
